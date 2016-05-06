0e4ef3547f9901d3d2ee8c42199645859814d053
oldwan-fly <shutaixiu@163.com>
2016-05-06 23:24:49 +0800

    css

---
+++ resume/js.js
@@ -0,0 +1,19 @@
+
+function showtext(){
+	var more1=document.getElementById("more1");
+	var more2=document.getElementById("more2");
+	var more3=document.getElementById("more3");
+	
+	more1.style.display="block";
+	more2.style.display="block";
+	more3.style.display="block";
+};
+function hidntext(){
+	var more1=document.getElementById("more1");
+	var more2=document.getElementById("more2");
+	var more3=document.getElementById("more3");
+	
+	more1.style.display="none";
+	more2.style.display="none";
+	more3.style.display="none";
+}
